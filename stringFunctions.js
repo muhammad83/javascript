1b36c23c0792aae44711d7be4a15f8e793974986
Muhammad Qureshi <muhammad.qureshi@ymail.com>
2017-01-16 08:24:00 +0500

    more updates for day 11
    added a new bootstrap page which looks better, so can practice boot
    strap and javascript at the same time.

--- stringFunctions.js
+++ stringFunctions.js
@@ -9,3 +9,19 @@ function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
 
 // Change the words here to test your function
 wordBlanks("dog", "big", "ran", "quickly");
+
+function helloWorld(){
+    //- Using a function pointer:
+
+    alert("congrats you have done hello world.")
+}
+
+function testFunction(){
+  // alert("inside test function");
+  var noun = document.getElementById("clickMe").value;
+  var whateve = wordBlanks(noun, "big", "ran", "quickly");
+
+  document.getElementById("clickMe").value = whateve;
+}
+
+// document.getElementById("testButton").onclick = helloWorld();
