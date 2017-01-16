ee9215dcff20ff9de8b0ace7bd91bfbc0486b859
Muhammad Qureshi <muhammad.qureshi@ymail.com>
2017-01-16 09:49:18 +0500

    part 2 of the multidimensional function

--- stringFunctions.js
+++ stringFunctions.js
@@ -22,4 +22,21 @@ function testFunction(){
   document.getElementById("clickMe").value = whateve;
 }
 
+function arrayMeBaby(arrayOne, arrayTwo, arrayThree){
+  console.log("I am inside the arrayMeBaby function");
+  var multiDemArry = arrayOne+arrayTwo+arrayThree;
+
+  return multiDemArry;
+}
+
+function makeMagicHappen(){
+  var arry1 = document.getElementById("array1").value; 
+  var arry2 = document.getElementById("array2").value;
+  var arry3 = document.getElementById("array3").value;
+  var result = arrayMeBaby(arry1, arry2, arry3);
+  console.log("inside the makeMagicHappen function and the values of arry1 is "+arry1+" arry2 is "+ arry2 +" and arry3 is "+ arry3);
+  console.log("the result returned by arrayMeBaby function is " +result);
+  document.getElementById("magic").value = result;
+}
+
 // document.getElementById("testButton").onclick = helloWorld();
