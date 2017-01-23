4ebae19041d9c71633c72c8f6e254914c3ce4cb7
Muhammad Qureshi <muhammad.qureshi@ymail.com>
2017-01-23 23:37:04 +0500

    added the golf par function
    haven’t added the html to use this javascript function

--- stringFunctions.js
+++ stringFunctions.js
@@ -75,3 +75,27 @@ function testLogicFunction(){
 
   document.getElementById("comparisonResult").value = result;
 }
+
+
+function golfScore(par, strokes) {
+  if (strokes === 1)
+    return "Hole-in-one!";
+
+  else if (strokes <= par - 2)
+    return "Eagle";
+  
+  else if (strokes === par - 1)
+    return "Birdie";
+  
+  else if (strokes === par)
+    return "Par";
+  
+  else if (strokes === par + 1)
+    return "Bogey";
+  
+  else if (strokes === par + 2)
+    return "Double Bogey";
+  
+  else if (strokes >= par + 3)
+    return "Go Home!";
+}
