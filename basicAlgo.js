f340a9e8dc4c84e6cc3373bb21aff36da91e4963
Muhammad Qureshi <muhammad.qureshi@ymail.com>
2017-03-09 15:38:37 +0000

    Ajax calls over jquery

--- basicAlgo.js
+++ basicAlgo.js
@@ -332,3 +332,19 @@ function rot13Runner(){
   rot13("SERR YBIR?");
   rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.");
 }
+
+function getquotes(){
+  $.ajaxSetup({
+  headers: {
+      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+});
+  $.getJSON(
+    'https://andruxnet-random-famous-quotes.p.mashape.com/cat=', 
+    function(data) { 
+      console.log (data);
+     }
+);
+}
